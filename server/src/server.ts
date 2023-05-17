f2e73eaec9507df2219c44b50c31beeb7f221e8d
IgorAugust0 <augustoigor269@gmail.com>
2023-05-17 09:13:30 -0300

    including missing dependencies/modules

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,13 @@
-const nome:string = "Igor";
-console.log("Hello World");
-console.log(`Meu nome é ${nome}`);
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.get('/hello', () => {
+    return 'Hello World'
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+})
