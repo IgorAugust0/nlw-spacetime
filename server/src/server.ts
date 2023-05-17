34a89a9d0cb9f5c69bf1bde2cef0b9166263f0c7
IgorAugust0 <augustoigor269@gmail.com>
2023-05-17 15:16:53 -0300

    add web directory and React + Next.js project

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,14 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient() // conexão com o banco
 
-app.get('/hello', () => {
-  return 'Hello World'
+// rota
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users
 })
 
 app
