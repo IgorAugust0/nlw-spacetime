64b8ee8ecb9d9665efeacc2bfca2248f75bbf2c9
IgorAugust0 <augustoigor269@gmail.com>
2023-05-19 12:19:41 -0300

    empty memories list in home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,23 @@
 // TSX é uma extensão do JSX, que é uma extensão do JS que permite escrever HTML dentro do JS
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-      <h1 className="font-alt text-4xl font-bold">Sua cápsula do tempo</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div className="flex flex-col items-start justify-between px-28 py-16"></div>
+
+      {/* Right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center"></div>
+        {/* leading-relaxed = line-height: 1.625; */}
+        {/* w-[360px] = width: 360px; */}
+        <p className="w-[360px] text-center leading-relaxed">
+          Você ainda não registrou nenhuma lembrança, comece a{' '}
+          <a href="" className="underline hover:text-gray-50">
+            criar agora
+          </a>
+          !
+        </p>
+      </div>
+    </main>
   )
 }
