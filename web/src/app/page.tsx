41fe0d5fddd64fdc0d2eb622a8617b18610a419c
IgorAugust0 <augustoigor269@gmail.com>
2023-05-19 14:31:47 -0300

    Update page.tsx

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,21 +4,21 @@ export default function Home() {
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden px-28 py-16">
-        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] translate-x-1/2 translate-y-1/2 rounded-full bg-purple-700 opacity-50 blur-full"></div>
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"></div>
       </div>
 
       {/* Right */}
       <div className="flex flex-col p-16">
-        <div className="flex flex-1 items-center justify-center"></div>
-        {/* leading-relaxed = line-height: 1.625; */}
-        {/* w-[360px] = width: 360px; */}
-        <p className="w-[360px] text-center leading-relaxed">
-          Você ainda não registrou nenhuma lembrança, comece a{' '}
-          <a href="" className="underline hover:text-gray-50">
-            criar agora
-          </a>
-          !
-        </p>
+        <div className="flex flex-1 items-center justify-center">
+          {/* leading-relaxed = line-height: 1.625; w-[360px] = width: 360px; */}
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
       </div>
     </main>
   )
