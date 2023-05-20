39c6dc1474f03fd5f2d2858afe67543e27dea453
IgorAugust0 <augustoigor269@gmail.com>
2023-05-20 17:34:55 -0300

    testing memories route and starting crud operations for memories

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -3,14 +3,25 @@ import { prisma } from '../lib/prisma'
 
 // Route: GET: /memories   // parameters in TS must have a type defined
 export async function memoriesRouteHandler(app: FastifyInstance) {
-  app.get('/users', async () => {
-    const users = await prisma.user.findMany()
-    // const users = await prisma.user.findMany({
-    //   select: {
-    //     name: true,
-    //   },
-    // })
+  app.get('/memories', async () => {
+    const memories = await prisma.memory.findMany({
+      orderBy: {
+        createdAt: 'asc', // sort by createdAt in ascending order
+      },
+    })
 
-    return users
+    // map memories to a new array of objects with only the properties we want
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        excerpt: memory.content.slice(0, 120).concat('...'), // slice the first 120 characters of the content and add ellipsis
+      }
+    })
   })
+
+  app.get('/memories/:id', async () => {})
+  app.post('/memories/', async () => {})
+  app.put('/memories/:id', async () => {})
+  app.delete('/memories/:id', async () => {})
 }
