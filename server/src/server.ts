4f94cfb2c41a89f38f12692786a2461626ea5624
IgorAugust0 <augustoigor269@gmail.com>
2023-05-21 02:59:29 -0300

    testing fetching oauth with axios

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,18 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRouteHandler } from './routes/memories'
+import { authRoutesHandler } from './routes/auth'
 
 // fastify instance
 const app = fastify()
 app.register(cors, {
   origin: true,
 })
-// fastify method to register a seoarate route file/handler
+// fastify method to register a separate route for the auth
+app.register(authRoutesHandler)
+// fastify method to register a separate route for the memories
 app.register(memoriesRouteHandler)
 
 // fastify method to start the server on a specific port
