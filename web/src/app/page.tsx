833313fe5cc783e479ae7f67f7212c6c5d4bfbbf
IgorAugust0 <augustoigor269@gmail.com>
2023-05-21 02:15:21 -0300

    turned divs from the page into components in web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,77 +1,26 @@
 // TSX é uma extensão do JSX, que é uma extensão do JS que permite escrever HTML dentro do JS
-import { User } from 'lucide-react' // Importa os ícones do Lucide
-import Image from 'next/image' // Importa o componente Image do Next.js e o coloca na PROPRIEDADE (Props) Image
-import nlwLogo from '../assets/nlw-spacetime-logo.svg' // Importa o logo do NLW e o coloca na variável nlwLogo
+import { Blur } from '@/components/Blur'
+import { Copyright } from '@/components/Copyright'
+import { EmptyMemories } from '@/components/EmptyMemories'
+import { Hero } from '@/components/Hero'
+import { SignIn } from '@/components/SignIn'
+import { Stripes } from '@/components/Stripes'
 
 export default function Home() {
   return (
     <main className="grid min-h-screen grid-cols-2">
       {/* Left */}
       <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] px-28 py-16">
-        {/* Blur */}
-        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"></div>
-        {/* Stripes */}
-        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
-        {/* Sign in */}
-        <a
-          href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
-          className="flex items-center gap-3 text-left "
-        >
-          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
-            <User className="h-5 w-5 text-gray-500" />
-          </div>
-          <p className="max-w-[148px] text-sm leading-snug transition-colors hover:text-gray-50">
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias!
-          </p>
-        </a>
-
-        {/* Hero */}
-        <div className="space-y-5">
-          <Image src={nlwLogo} alt="NLW Spacetime" />
-          <div className="max-w-[420px] space-y-1">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-            <p className="text-lg leading-relaxed">
-              Colecione momentos marcantes da sua jornada e compartilhe (se
-              quiser) com o mundo!
-            </p>
-          </div>
-          <a
-            className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black transition-colors hover:bg-green-600"
-            href=""
-          >
-            cadastrar lembrança
-          </a>
-        </div>
-
-        {/* Copyright */}
-        <div className="text-sm leading-relaxed text-gray-200">
-          Feito com 💜 no NLW da{' '}
-          <a
-            target="_blank"
-            rel="noreferrer"
-            className="underline hover:text-gray-100"
-            href="https://rocketseat.com.br"
-          >
-            Rocketseat
-          </a>
-        </div>
+        <Blur />
+        <Stripes />
+        <SignIn />
+        <Hero />
+        <Copyright />
       </div>
 
       {/* Right */}
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <div className="flex flex-1 items-center justify-center">
-          {/* leading-relaxed = line-height: 1.625; w-[360px] = width: 360px; */}
-          <p className="w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, comece a{' '}
-            <a href="" className="underline hover:text-gray-50">
-              criar agora
-            </a>
-            !
-          </p>
-        </div>
+        <EmptyMemories />
       </div>
     </main>
   )
---
+++ web/src/components/EmptyMemories.tsx
@@ -0,0 +1,14 @@
+export function EmptyMemories() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      {/* leading-relaxed = line-height: 1.625; w-[360px] = width: 360px; */}
+      <p className="w-[360px] text-center leading-relaxed">
+        Você ainda não registrou nenhuma lembrança, comece a{' '}
+        <a href="" className="underline hover:text-gray-50">
+          criar agora
+        </a>
+        !
+      </p>
+    </div>
+  )
+}
