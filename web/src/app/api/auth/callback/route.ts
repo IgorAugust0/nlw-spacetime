9eeeeff90d5aad1d7d06347a68b1831b34da6b46
IgorAugust0 <augustoigor269@gmail.com>
2023-09-08 17:37:55 -0300

    add web middleware

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -5,6 +5,8 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
 
+  const redirectTo = request.cookies.get('redirectTo')?.value || '/'
+
   const registerResponse = await api.post('/register', {
     code,
   })
@@ -12,7 +14,7 @@ export async function GET(request: NextRequest) {
   const { token } = registerResponse.data
 
   // redirect to the home (root) page and set the token in a cookie
-  const redirectURL = new URL('/', request.url)
+  const redirectURL = redirectTo ?? new URL('/', request.url) // if redirectTo is not defined, redirect to the home (root) page
 
   // in seconds
   const cookieExpiration = 60 * 60 * 24 * 30 // 30 days
---
+++ web/src/middleware.ts
@@ -0,0 +1,29 @@
+// Middleware is used to intercept requests and responses to perform some action,
+// like preventing an unauthenticated user from accessing a protected route,
+// available only to authenticated users, for instance.
+// There are two types of middleware: client-side and server-side.
+// The client-side middleware is used to intercept requests and responses on the client-side,
+// while the server-side middleware is used to intercept requests and responses on the server-side.
+
+import { NextRequest, NextResponse } from 'next/server'
+
+const signInUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}` // define the url to redirect to if the user is not authenticated
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('token')?.value // get the token from the cookie, if it exists thats why the ?. operator is used
+
+  if (!token) {
+    return NextResponse.redirect(signInUrl, {
+      headers: {
+        'Set-Cookie': `redirectTo=${request.url}; Path=/; HttpOnly; max-age=17;`,
+      },
+    }) // redirect to login page if token is not found, and after login redirect back to the page the user was trying to access
+  }
+
+  return NextResponse.next() // continue to the next request if token is found
+}
+
+export const config = {
+  matcher: '/memories/:path*', // match all routes starting with /memories
+  //   matcher: /^\/api\/.*/,
+}
